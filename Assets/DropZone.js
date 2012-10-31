5a3822340854b3b07f945641c7e63cdd960422f2
Patrick-Ranjit D. Madsen <pamad05+github@gmail.com>
2012-10-31 22:00:13 +0100

    # DropZone units - Define DropZones as either being Bug or Soldier DropZones - Set the interval with which to spawn them - selectable.js was edited to search for tags "dropzone" and update the target (obviously, the player will only be able to change the target on his own DropZones). - working towards a sort of NextNode thing from each Node.. - point is: we now have units running towards each other :)
    TODO: lineOfSight - coming right up garçon!

---
+++ Assets/DropZone.js
@@ -0,0 +1,21 @@
+#pragma strict
+var spawnTimer : float = 3;
+var Soldier : Transform;
+var Target : Transform;
+
+function Start () {
+	
+}
+
+function Update () {
+	if	( spawnTimer > 0 ) {
+		spawnTimer -= Time.deltaTime;
+ 	} else {
+ 		spawnTimer = 3;
+  		Debug.Log("SPAWNING!");
+  		var soldier : Transform = Instantiate(Soldier, transform.position, Quaternion.Euler(0,90,0)); 
+		soldier.GetComponent(AIPath).target = Target;
+ 	}
+ 	
+ 	return;
+}
