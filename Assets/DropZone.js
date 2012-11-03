1bf9d686072e5c4a5863361fd445c4041811046b
Patrick-Ranjit D. Madsen <pamad05+github@gmail.com>
2012-11-03 12:33:16 +0100

    Improved LoS - units scan for enemies continuously until an enemy is discovered - unit cease scanning and engages the enemy until enemy is either dead or out of range - when enemy is out of range, unit resumes scanning

--- Assets/DropZone.js
+++ Assets/DropZone.js
@@ -4,18 +4,19 @@ var Soldier : Transform;
 var Target : Transform;
 
 function Start () {
-	
+	InvokeRepeating("SpawnUnit", 3.0, 3.0);
 }
 
-function Update () {
-	if	( spawnTimer > 0 ) {
-		spawnTimer -= Time.deltaTime;
-		return;
- 	}
- 	spawnTimer = 3;
-  	Debug.Log("SPAWNING!");
+function SpawnUnit() {
+	Debug.Log("SPAWNING!");
   	var soldier : Transform = Instantiate(Soldier, transform.position, Quaternion.Euler(0,90,0)); 
 	soldier.GetComponent(AIPath).target = Target;
+}
+
+
+function Update () {
+	
+}ath).target = Target;
  	
  	return;
 }
