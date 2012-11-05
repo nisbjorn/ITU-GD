38f62d1e05673e9a71674204ff2b82cf380cef86
Nis Bjørn <nisbjorn@gmail.com>
2012-11-05 15:52:07 +0100

    Sorte labels så man kan se hvad der står. Har eksperimenteret med firingfrequency, og den virker ikke. Jeg har yield

--- Assets/units/GUILabel.js
+++ Assets/units/GUILabel.js
@@ -5,5 +5,6 @@ function OnGUI() {
 	pos.y += 2;
 	pos = Camera.main.WorldToScreenPoint(pos); 
 	var rect = new Rect(pos.x - 10, Screen.height - pos.y - 15, 100, 22);
+	GUI.color = Color.black;
     GUI.Label(rect, this.GetComponent(unitLogic).baseHealth.ToString());
 }
